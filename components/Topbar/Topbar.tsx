727bca58cf837462bff3e9c5caa03751568aeaa0
Jonmari Dandal <jonmari.dandal.solid@gmail.com>
2023-07-04 18:50:56 +0800

    Reset Password and user icon

--- components/Topbar/Topbar.tsx
+++ components/Topbar/Topbar.tsx
@@ -1,10 +1,20 @@
+import { authModalState } from "@/atoms/authModalAtom";
+import { auth } from "@/firebase/firebase";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useSetRecoilState } from "recoil";
+import Logout from "../Buttons/Logout";
 
 type TopbarProps = {};
 
 const Topbar: React.FC<TopbarProps> = () => {
+    const [user] = useAuthState(auth);
+    const setAuthModalState = useSetRecoilState(authModalState);
+    const router = useRouter();
+
     return (
         <nav className="relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7">
             <div className={`flex w-full items-center justify-between`}>
@@ -28,11 +38,41 @@ const Topbar: React.FC<TopbarProps> = () => {
                             Premium
                         </a>
                     </div>
-                    <Link href="/auth">
-                        <button className="bg-dark-fill-3 py-1 px-2 cursor-pointer rounded">
-                            Sign In
-                        </button>
-                    </Link>
+                    {!user && (
+                        <Link
+                            href="/auth"
+                            onClick={() =>
+                                setAuthModalState((prev) => ({
+                                    ...prev,
+                                    isOpen: true,
+                                    type: "login",
+                                }))
+                            }
+                        >
+                            <button className="bg-dark-fill-3 py-1 px-2 cursor-pointer rounded">
+                                Sign In
+                            </button>
+                        </Link>
+                    )}
+                    {user && (
+                        <div className="cursor-pointer group relative">
+                            <Image
+                                src="/avatar.png"
+                                alt="Avatar"
+                                width={30}
+                                height={30}
+                                className="rounded-full"
+                            />
+                            <div
+                                className="absolute top-10 left-2/4 -translate-x-2/4  mx-auto bg-dark-layer-1 text-brand-orange p-2 rounded shadow-lg 
+								z-40 group-hover:scale-100 scale-0 
+								transition-all duration-300 ease-in-out"
+                            >
+                                <p className="text-sm">{user.email}</p>
+                            </div>
+                        </div>
+                    )}
+                    {user && <Logout />}
                 </div>
             </div>
         </nav>
