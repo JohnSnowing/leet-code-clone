fcbcd9fc0acdbf7d10ed280f95203922e0a74da5
Jonmari Dandal <jonmari.dandal.solid@gmail.com>
2023-07-18 16:10:37 +0800

    Handle dislikes and starred functionality

--- components/Workspace/ProblemDescription/ProblemDescription.tsx
+++ components/Workspace/ProblemDescription/ProblemDescription.tsx
@@ -2,7 +2,14 @@ import CircleSkeleton from "@/components/Skeletons/CircleSkeleton";
 import RectangleSkeleton from "@/components/Skeletons/RectangleSkeleton";
 import { auth, firestore } from "@/firebase/firebase";
 import { DBProblem, Problem } from "@/utils/types/problem";
-import { doc, getDoc, runTransaction } from "firebase/firestore";
+import {
+    arrayRemove,
+    arrayUnion,
+    doc,
+    getDoc,
+    runTransaction,
+    updateDoc,
+} from "firebase/firestore";
 import Image from "next/image";
 import React, { useState, useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
@@ -130,9 +137,112 @@ const ProblemDescription: React.FC<ProblemDescriptionProps> = ({
         setUpdating(false);
     };
 
-    const handleDislike = () => {};
+    const handleDislike = async () => {
+        if (!user) {
+            toast.error("You must be logged in to dislike a problem", {
+                position: "top-left",
+                theme: "dark",
+            });
+            return;
+        }
+        if (updating) return;
+        setUpdating(true);
+        await runTransaction(firestore, async (transaction) => {
+            const { problemDoc, userDoc, problemRef, userRef } =
+                await returnUserDataAndProblemData(transaction);
+            if (userDoc.exists() && problemDoc.exists()) {
+                // already disliked, already liked, not disliked or liked
+                if (disliked) {
+                    transaction.update(userRef, {
+                        dislikedProblems: userDoc
+                            .data()
+                            .dislikedProblems.filter(
+                                (id: string) => id !== problem.id,
+                            ),
+                    });
+                    transaction.update(problemRef, {
+                        dislikes: problemDoc.data().dislikes - 1,
+                    });
+                    setCurrentProblem((prev) =>
+                        prev ? { ...prev, dislikes: prev.dislikes - 1 } : null,
+                    );
+                    setData((prev) => ({ ...prev, disliked: false }));
+                } else if (liked) {
+                    transaction.update(userRef, {
+                        dislikedProblems: [
+                            ...userDoc.data().dislikedProblems,
+                            problem.id,
+                        ],
+                        likedProblems: userDoc
+                            .data()
+                            .likedProblems.filter(
+                                (id: string) => id !== problem.id,
+                            ),
+                    });
+                    transaction.update(problemRef, {
+                        dislikes: problemDoc.data().dislikes + 1,
+                        likes: problemDoc.data().likes - 1,
+                    });
+                    setCurrentProblem((prev) =>
+                        prev
+                            ? {
+                                  ...prev,
+                                  dislikes: prev.dislikes + 1,
+                                  likes: prev.likes - 1,
+                              }
+                            : null,
+                    );
+                    setData((prev) => ({
+                        ...prev,
+                        disliked: true,
+                        liked: false,
+                    }));
+                } else {
+                    transaction.update(userRef, {
+                        dislikedProblems: [
+                            ...userDoc.data().dislikedProblems,
+                            problem.id,
+                        ],
+                    });
+                    transaction.update(problemRef, {
+                        dislikes: problemDoc.data().dislikes + 1,
+                    });
+                    setCurrentProblem((prev) =>
+                        prev ? { ...prev, dislikes: prev.dislikes + 1 } : null,
+                    );
+                    setData((prev) => ({ ...prev, disliked: true }));
+                }
+            }
+        });
+        setUpdating(false);
+    };
 
-    const handleStar = () => {};
+    const handleStar = async () => {
+        if (!user) {
+            toast.error("You must be logged in to star a problem", {
+                position: "top-left",
+                theme: "dark",
+            });
+            return;
+        }
+        if (updating) return;
+        setUpdating(true);
+
+        if (!starred) {
+            const userRef = doc(firestore, "users", user.uid);
+            await updateDoc(userRef, {
+                starredProblems: arrayUnion(problem.id),
+            });
+            setData((prev) => ({ ...prev, starred: true }));
+        } else {
+            const userRef = doc(firestore, "users", user.uid);
+            await updateDoc(userRef, {
+                starredProblems: arrayRemove(problem.id),
+            });
+            setData((prev) => ({ ...prev, starred: false }));
+        }
+        setUpdating(false);
+    };
 
     return (
         <div className="bg-dark-layer-1">
